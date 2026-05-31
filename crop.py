40dabbdea88e8497d9698f0aac46892425713481
trgxxxxx-del <trgxxxxx@gmail.com>
2026-05-30 22:39:50 -0300

    Update crop.py

--- crop.py
+++ crop.py
@@ -5,7 +5,7 @@ from datetime import datetime, timezone
 from pathlib import Path
 
 URL  = "https://cdn.star.nesdis.noaa.gov/GOES19/ABI/SECTOR/ssa/GEOCOLOR/7200x4320.jpg"
-CROP = (2730, 1360, 2986, 1620)
+CROP = (2679, 1344, 2985, 1639)
 OUT  = Path("tucuman.png")
 META = Path("last_update.txt")
 
