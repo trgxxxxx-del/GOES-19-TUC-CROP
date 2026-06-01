5eb6732cf5f6ea1a36eee143ceb187c41c7cf0d5
trgxxxxx-del <trgxxxxx@gmail.com>
2026-05-31 22:07:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ st.markdown("""
 st.title("🛰️ Imágen satelital de Tucumán")
 
 URL       = "https://cdn.star.nesdis.noaa.gov/GOES19/ABI/SECTOR/ssa/GEOCOLOR/7200x4320.jpg"
-CROP = CROP = (2620, 1290, 2835, 1506)
+CROP = (2570, 1290, 2785, 1506)
 THRESHOLD = 128
 MAT_PATH  = Path("matriz de departamentos.xlsx")
 
