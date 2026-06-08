93f5c1d0e403233a4b629bd30dc3136fe5346dd3
trgxxxxx-del <trgxxxxx@gmail.com>
2026-06-07 21:47:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ URL_GEOCOLOR = "https://cdn.star.nesdis.noaa.gov/GOES19/ABI/SECTOR/ssa/GEOCOLOR/
 URL_NIGHT    = "https://cdn.star.nesdis.noaa.gov/GOES19/ABI/SECTOR/ssa/16/7200x4320.jpg"
 CROP            = (2717, 1382, 2932, 1600)
 THRESHOLD_DIA   = 110
-THRESHOLD_NOCHE = 170
+THRESHOLD_NOCHE = 110
 MAT_PATH        = Path("matriz de departamentos.xlsx")
 TZ_ARG          = timezone(timedelta(hours=-3))
 
