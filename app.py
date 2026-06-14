e9fb27f16f3c5517e30bd07e256997f7e17f4f09
trgxxxxx-del <trgxxxxx@gmail.com>
2026-06-13 23:30:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -147,7 +147,7 @@ try:
         st.cache_data.clear()
         st.rerun()
 
-    col_img, col_tabla = st.columns([2, 2])
+    col_img, col_tabla = st.columns([2, 1])
 
     with col_img:
         st.image(crop_geo, use_container_width=True)
